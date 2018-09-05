637cdf89a2b4d335991c8530bfec25974133be19
nuttapon <nuttapon.k@dots.co.th>
2018-09-05 15:58:24 +0700

    Finish 5Sep

--- app/src/main/java/nuttapon/dots/co/th/dotssolutions/PackageFragment.java
+++ app/src/main/java/nuttapon/dots/co/th/dotssolutions/PackageFragment.java
@@ -7,12 +7,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PackageFragment extends Fragment {
+
+    private MyConstant myConstant=new MyConstant();
+    private MyAlert myAlert;
+    private String displayNameString;
 
 
     public PackageFragment() {
@@ -23,9 +29,34 @@ public class PackageFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        myAlert = new MyAlert(getActivity());
+
+//        Upload Controller
+        uploadController();
 
 
     }  // Main Method
+
+    private void uploadController() {
+        Button button = getView().findViewById(R.id.btnUpload);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                Get Value From EditText
+                EditText editText=getView().findViewById(R.id.editDisplayName);
+                displayNameString=editText.getText().toString().trim();
+
+                if (displayNameString.isEmpty()){         // เช็คว่ามีค่าว่างเปล่ามั้ย
+                    myAlert.normalDialog(getString(R.string.title_have_space),
+                            getString(R.string.title_have_space));
+                }
+
+
+
+            } // onClick
+        });
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
